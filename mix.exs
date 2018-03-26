1d0169929f5e132306ea36b802f9ea588188133b
Marcos Benatti Lauer <benatti77@gmail.com>
2018-03-26 00:49:01 -0300

    Mix exs + user controller test

--- mix.exs
+++ mix.exs
@@ -5,16 +5,20 @@ defmodule GithubTags.MixProject do
     [
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
       deps: deps()
     ]
   end
 
-  # Dependencies listed here are available only for this
-  # project and cannot be accessed from applications inside
-  # the apps folder.
-  #
-  # Run "mix help deps" for examples and options.
   defp deps do
     []
+  end
+
+  defp aliases do
+    [
+     "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+     "ecto.reset": ["ecto.drop", "ecto.setup"],
+     "test":       ["ecto.create --quiet", "ecto.migrate", "test"]
+    ]
   end
 end

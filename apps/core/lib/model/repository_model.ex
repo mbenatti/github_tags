b24018a143d00c00d9376cca0e5fe22bedba1be4
Marcos Benatti Lauer <benatti77@gmail.com>
2018-03-26 00:14:12 -0300

    Model/Logic for repository and user

---
+++ apps/core/lib/model/repository_model.ex
@@ -0,0 +1,153 @@
+defmodule GithubTags.Model.Repository do
+  @moduledoc"""
+  Helper Module to interact with Repository Schema and Database
+  """
+
+  alias GithubTags.Repo
+
+  alias GithubTags.Schemas.Repository, as: RepositorySchema
+  alias GithubTags.Model.User
+
+  import Ecto.Query, only: [from: 2]
+  import Ecto.Changeset, only: [change: 1, put_change: 3]
+
+  @repo_not_found "repository not found"
+  @user_not_found "user not found"
+
+  @doc """
+  Populate or update repositories for a given username
+
+  ##Example
+      iex()> GithubTags.Model.Repository.populate_or_update_repositories("mbenatti")
+      :ok
+  """
+  @spec populate_or_update_repositories(String.t) :: :ok
+  def populate_or_update_repositories(username) do
+    case User.get_user(username) do
+      nil -> @user_not_found
+      _user ->
+        github_request(username)
+        |> Task.async_stream(fn repository ->
+          add_or_update(username, %{
+            "name" => repository["name"],
+            "description" => repository["description"],
+            "language" => repository["language"],
+            "html_url" => repository["html_url"]
+          })
+        end)
+        |> Stream.run()
+    end
+  end
+
+  defp github_request(username) do
+    case Tentacat.Users.Starring.starred(username, Tentacat.Client.new) do
+      {404, _} -> []
+      result -> result
+    end
+  end
+
+  defp add_or_update(username, %{"name" => name, "description" => desc, "language" => lang, "html_url" => url}) do
+    case get_by_url(username, url) do
+      nil -> %RepositorySchema{user: username}
+      repository -> repository
+    end
+    |> RepositorySchema.changeset(%{"name" => name, "description" => desc, "language" => lang, "url" => url})
+    |> Repo.insert_or_update
+  end
+
+  @doc """
+  Get all repositories by username
+  """
+  @spec get_by_user(String.t) :: [RepositorySchema.t] | []
+  def get_by_user(username) do
+    from(r in RepositorySchema, where: r.user == ^username)
+    |> Repo.all()
+  end
+
+  def get_by_tag(username, tag) do
+    from(r in RepositorySchema, where: r.user == ^username and ^tag in r.tags)
+    |> Repo.all()
+  end
+
+  @doc """
+  Get all existing repositories
+  """
+  @spec get_all() :: [RepositorySchema.t] | []
+  def get_all() do
+    Repo.all(RepositorySchema)
+  end
+
+  @doc """
+  Get a User repository by url
+  """
+  @spec get_by_url(String.t, String.t) :: RepositorySchema.t | nil
+  def get_by_url(username, repository_url) do
+    Repo.get_by(RepositorySchema, user: username, url: repository_url)
+  end
+
+  @doc """
+  Add a tag
+  """
+  @spec add_tag(String.t, Integer.t, String.t) :: String.t | {:ok, RepositorySchema.t}
+  def add_tag(username, repository_id, tag) do
+    case Repo.get_by(RepositorySchema, user: username, id: repository_id) do
+      nil ->
+        @repo_not_found
+      repository ->
+        add_tag(repository, tag)
+    end
+  end
+
+  @doc """
+  Add a tag by repository url
+  """
+  @spec add_tag_by_url(String.t, String.t, String.t) :: String.t | {:ok, RepositorySchema.t}
+  def add_tag_by_url(username, repository_url, tag) do
+    case get_by_url(username, repository_url) do
+      nil ->
+        @repo_not_found
+      repository ->
+        add_tag(repository, tag)
+    end
+  end
+  defp add_tag(repository, tag) do
+    case repository.tags do
+      nil ->
+        repository
+        |> change()
+        |> put_change(:tags, [tag])
+        |> Repo.update
+      tags ->
+        if !Enum.member?(tags, tag) do
+          repository
+          |> change()
+          |> put_change(:tags, [tag | tags])
+          |> Repo.update
+        else
+          {:ok, repository}
+        end
+    end
+  end
+
+  @doc """
+  Remove a tag by repository url
+  """
+  @spec remove_tag(String.t, String.t, String.t) :: String.t | {:ok, RepositorySchema.t}
+  def remove_tag(username, repository_url, tag) do
+    repository = get_by_url(username, repository_url)
+    case repository.tags do
+      nil ->
+        @repo_not_found
+      _tags ->
+        remove_tag(repository, tag)
+    end
+  end
+  defp remove_tag(repository, tag) do
+    new_tag_list = List.delete(repository.tags, tag)
+
+    repository
+    |> change()
+    |> put_change(:tags, new_tag_list)
+    |> Repo.update
+  end
+end
---
+++ apps/core/lib/model/user_model.ex
@@ -0,0 +1,58 @@
+defmodule GithubTags.Model.User do
+  @moduledoc"""
+  Helper Module to interact with User Schema and Database
+  """
+
+  alias GithubTags.Repo
+  alias GithubTags.Schemas.User, as: UserSchema
+
+  @doc """
+  Get or create an User Create an User
+
+  ## Examples
+
+      iex()> GithubTags.Model.User.get_or_create_user("mbenatti")
+      %GithubTags.Schemas.User{
+        __meta__: #Ecto.Schema.Metadata<:loaded, "users">,
+        inserted_at: ~N[2018-03-24 23:00:08.962249],
+        repositories: #Ecto.Association.NotLoaded<association :repositories is not loaded>,
+        updated_at: ~N[2018-03-24 23:00:08.962274],
+        username: "mbenatti"
+      }
+
+  """
+  @spec get_or_create_user(String.t) :: UserSchema.t
+  def get_or_create_user(username) do
+    case Repo.get(UserSchema, username) do
+      nil -> create_user(username)
+      user -> user
+    end
+  end
+
+  @doc """
+  Get a user
+  """
+  @spec get_user(String.t) :: UserSchema.t
+  def get_user(username) do
+    Repo.get(UserSchema, username)
+  end
+
+  @doc """
+  Get all users
+  """
+  @spec get_users() :: [UserSchema.t]
+  def get_users() do
+    Repo.all(UserSchema)
+  end
+
+  @doc """
+  Create an User Create na User
+  """
+  @spec create_user(String.t) :: UserSchema.t | {:error, UserSchema.Changeset.t}
+  def create_user(username) do
+    case Repo.insert(%UserSchema{username: username}) do
+      {:ok, schema} -> schema
+      {:error, changeset} -> {:error, changeset}
+    end
+  end
+end
